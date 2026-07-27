2081a42ec27d6c288ab701325397bb3c9e1c6442
meekehm <135926575+meekehm@users.noreply.github.com>
2026-07-27 16:59:58 -0700

    Refactor ShoppingList class and update UI components
    Removed unused includes and item struct. Added UI components for shopping list functionality.

--- PantryPal/shoppinglist.h
+++ PantryPal/shoppinglist.h
@@ -1,23 +1,10 @@
-#pragma once
 #ifndef SHOPPINGLIST_H
 #define SHOPPINGLIST_H
 
 #include <QWidget>
-#include <QVector>
-#include <QString>
-#include <QDate>
-
-struct Item
-{
-    QString name;
-    int quantity;
-    int minimumQuantity;
-    QDate expirationDate;
-};
-
-namespace Ui {
-    class ShoppingList;
-}
+#include <QListWidget>
+#include <QPushButton>
+#include <QLabel>
 
 class ShoppingList : public QWidget
 {
@@ -25,16 +12,16 @@ class ShoppingList : public QWidget
 
 public:
     explicit ShoppingList(QWidget* parent = nullptr);
-    ~ShoppingList();
 
-    void loadPantryItems(const QVector<Item>& items);
+private slots:
+    void refreshShoppingList();
+    void removePurchased();
 
 private:
-    Ui::ShoppingList* ui;
-
-    QVector<Item> pantryItems;
-
-    void generateShoppingList();
+    QLabel* title;
+    QListWidget* listWidget;
+    QPushButton* refreshButton;
+    QPushButton* removeButton;
 };
 
 #endif
